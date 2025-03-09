af73d439e5cd3444dd0668b4caea62678e842709
kenwallace808 <kenwallace808@gmail.com>
2025-03-09 13:14:55 -1000

    Got React talking to Node
    Got React talking to Node

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -1,11 +1,24 @@
-const { createServer } = require('node:http');
-const hostname = '127.0.0.1';
+//const { createServer } = require('node:http');
+//const hostname = '127.0.0.1';
 const port = 3000;
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// const server = createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+const express = require('express')
+var cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello Moto')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
