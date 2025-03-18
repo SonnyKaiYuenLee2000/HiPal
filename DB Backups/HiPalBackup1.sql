acd3d8f7d666a57627bfd89897cffb6d2386dabf
kenwallace808 <kenwallace808@gmail.com>
2025-03-17 21:21:16 -1000

    Update HiPalBackup1.sql
    New Tables:
    rfrPointType (Added some base point types: HIKE, SWIM, DINE, CHILL)
    tblPointToType (Added three rows for alas and a point for my old magellan house)
    
    Other:
    tblPoints (Added point for ala moana beach park)

--- DB Backups/HiPalBackup1.sql
+++ DB Backups/HiPalBackup1.sql
@@ -16,6 +16,29 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `rfrPointType`
+--
+
+DROP TABLE IF EXISTS `rfrPointType`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rfrPointType` (
+  `strPointType` varchar(6) DEFAULT NULL,
+  `strDescription` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rfrPointType`
+--
+
+LOCK TABLES `rfrPointType` WRITE;
+/*!40000 ALTER TABLE `rfrPointType` DISABLE KEYS */;
+INSERT INTO `rfrPointType` VALUES ('SWIM','Swim'),('HIKE','Hike'),('DINE','Dining'),('CHILL','Chill');
+/*!40000 ALTER TABLE `rfrPointType` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tblPoints`
 --
 
@@ -23,7 +46,7 @@ DROP TABLE IF EXISTS `tblPoints`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tblPoints` (
-  `lngKey` bigint DEFAULT NULL,
+  `lngPointKey` bigint DEFAULT NULL,
   `strAddress` varchar(255) DEFAULT NULL,
   `strTitle` varchar(255) DEFAULT NULL,
   `strDescription` varchar(255) DEFAULT NULL,
@@ -37,8 +60,32 @@ CREATE TABLE `tblPoints` (
 
 LOCK TABLES `tblPoints` WRITE;
 /*!40000 ALTER TABLE `tblPoints` DISABLE KEYS */;
-INSERT INTO `tblPoints` VALUES (1,'318 Magellan Ave Honolulu HI 96813','The Magellan House','Commissioners stomping grounds',_binary '\0\0\0\0\0\0\0\î|\Ü\n£O5@›b<C»cÀ');
+INSERT INTO `tblPoints` VALUES (1,'318 Magellan Ave Honolulu HI 96813','The Magellan House','Commissioners stomping grounds',_binary '\0\0\0\0\0\0\0\î|\Ü\n£O5@›b<C»cÀ'),(2,'1201 Ala Moana Blvd, Honolulu, HI 96814','Ala Moana Beach Park','Huge park with a plethora of things to do',_binary '\0\0\0\0\0\0\0J\"\ÞBJ5@5R\Ô»cÀ');
 /*!40000 ALTER TABLE `tblPoints` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tblPointToType`
+--
+
+DROP TABLE IF EXISTS `tblPointToType`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tblPointToType` (
+  `lngPointKey` bigint DEFAULT NULL,
+  `strPointType` varchar(6) DEFAULT NULL,
+  `strDescription` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tblPointToType`
+--
+
+LOCK TABLES `tblPointToType` WRITE;
+/*!40000 ALTER TABLE `tblPointToType` DISABLE KEYS */;
+INSERT INTO `tblPointToType` VALUES (2,'SWIM','1 mile out-and-back good for swim conditioning'),(2,'HIKE','7 mile walking path great for walking and running'),(2,'DINE','L&L: Local kine grindz'),(1,'CHILL','Kick it with cat boy');
+/*!40000 ALTER TABLE `tblPointToType` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -50,4 +97,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-16 15:08:12
+-- Dump completed on 2025-03-17 21:20:12
