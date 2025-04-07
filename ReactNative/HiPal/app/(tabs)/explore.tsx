6f88e31826f3afd2a06a1c845c4ce1bebf017474
sonnykaiyuenlee <sonny@uchicago.edu>
2025-04-06 16:46:10 -1000

    John and Sonny have conquered the world
    Got a map to show up that is bounded on ala moana beach, and fixed package dependencies issues

--- ReactNative/HiPal/app/(tabs)/explore.tsx
+++ ReactNative/HiPal/app/(tabs)/explore.tsx
@@ -1,13 +1,66 @@
-import { StyleSheet, Image, Platform } from 'react-native';
-
+import { StyleSheet, Image, Platform} from 'react-native';
+import { GoogleMap, Marker, MarkerProps, useJsApiLoader } from '@react-google-maps/api'
 import { Collapsible } from '@/components/Collapsible';
 import { ExternalLink } from '@/components/ExternalLink';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
+import { useState, useCallback } from 'react';
+
+const center = {
+  lat: 21.289965,
+  lng: -157.8544448,
+}
+
+const markerPosition = {
+  lat: 21.292008,
+  lng:-157.8479726,
+}
+
 
 export default function TabTwoScreen() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: 'AIzaSyD1SX9Mw--fY18OQX8MBKQCfOt2AXZCvYg',
+  })
+
+  const [map, setMap] = useState<google.maps.Map|null>(null)
+
+  const containerStyle = {
+    width: '400px',
+    height: '400px',
+  }
+  
+  const onLoad = useCallback(function callback(map:google.maps.Map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bothBounds = new google.maps.LatLngBounds(center, markerPosition)
+    const bounds = new window.google.maps.LatLngBounds(bothBounds)
+    map.fitBounds(bounds)
+
+    setMap(map)
+  }, [])
+
+  const onUnmount = useCallback(function callback(map:google.maps.Map) {
+    setMap(null)
+  }, [])
+
+  const renderMap = ()=>{
+    return isLoaded ? <GoogleMap
+    mapContainerStyle={containerStyle}
+    center={center}
+    zoom={10}
+    onLoad={onLoad}
+    onUnmount={onUnmount}
+  >
+    {/* Child components, such as markers, info windows, etc. */}
+    <Marker
+    position = { {
+      lat: 21.292008,
+      lng:-157.8479726,
+    }}/>
+  </GoogleMap> : <></>
+  }
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
@@ -23,74 +76,9 @@ export default function TabTwoScreen() {
         <ThemedText type="title">Explore</ThemedText>
       </ThemedView>
       <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user's current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
+      {
+        renderMap()
+      }
     </ParallaxScrollView>
   );
 }
